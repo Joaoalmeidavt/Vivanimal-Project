ec2166d5516392aa70f45386182bfabac7db4b19
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-12-18 21:23:49 +0000

    feat: create volunteer and add toastera

--- src/app/ajuda-voluntaria/page.tsx
+++ src/app/ajuda-voluntaria/page.tsx
@@ -1,32 +1,44 @@
 
-import Footer from '@/components/footer/Footer'
+'use client'
+import ButtonUI from '@/components/ButtonUI'
 import BecomeAVolunteer from '@/components/volunteers/BecomeAVolunteer'
+import FooterVolunteer from '@/components/volunteers/FooterVolunteer'
 import Mbway from '@/components/volunteers/MbWay'
-import VisiteForm from '@/components/volunteers/VisiteForm'
+
+import { BsFillTelephoneFill } from 'react-icons/bs'
 
 export default function Page() {
     return (
         <main className='h-full bg-gray-50 relative shadow'>
             <section className="container m-auto w-[90%] flex flex-col gap-8 justify-center items-center pt-8 pb-24">
                 <Mbway />
+                <ButtonUI classNames='bg-white shadow-md -mt-8' onPress={() => window.location.href = `tel:+351910022248`}
+                >Agende uma vista <BsFillTelephoneFill className="min-w-3 min-h-3" />
+                </ButtonUI>
 
-                <article>Agende uma vista - 999999999999
-              
-                </article>
-                <article>Seja um voluntário
-                    <BecomeAVolunteer />
-                </article>
+                <section className='bg-red-200 w-full justify-left items-center'>
+                    <h2 className="text-xl font-semibold text-blue-900 w-full">Saiba mais sobre como ajudar</h2>
 
-                necessidades:
-                produto de limpeza - lixivia****, detergente de louças, lava-tudo**, spray de tecido, detergente de roupas
+                    <p> -Nossas maiores necessidades - </p>
+                    <p>
+                        produto de limpeza - lixivia****, detergente de louças, lava-tudo**, spray de tecido, detergente de roupas
+                    </p>
 
 
 
-adotar - agendar visita - marcação telefonica - 
-voluntario usa o formulário
+                </section>
+                <BecomeAVolunteer />
+
+
+
+
+
+
+                adotar - agendar visita - marcação telefonica -
+                voluntario usa o formulário
 
             </section>
-            <Footer />
+            <FooterVolunteer />
         </main>
     )
 }
--- src/components/NavbarUI.tsx
+++ src/components/NavbarUI.tsx
@@ -2,23 +2,11 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/navbar";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-import AvatarUI from "./AvatarUI";
-import { IoIosSearch } from "react-icons/io";
-import Image from "next/image";
 
-export const AcmeLogo = () => {
-    return (
-        <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-            <path
-                clipRule="evenodd"
-                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-                fill="currentColor"
-                fillRule="evenodd"
-            />
-        </svg>
-    );
-};
+import Image from "next/image";
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+
 
 
 const menuItems = [
@@ -63,11 +51,18 @@ export default function NavbarUI() {
                     type="search"
                 /> */}
 
+
                 <NavbarContent justify="end">
                     <NavbarMenuToggle
-                        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                        className="sm:hidden"
-                        icon={<AvatarUI />}
+                        aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+                        className="text-brand-text-primary"
+                        icon={
+                            isMenuOpen ? (
+                                <IoMdClose className="w-8 h-8" />
+                            ) : (
+                                <IoMdMenu className="w-8 h-8" />
+                            )
+                        }
                     />
                 </NavbarContent>
                 <NavbarMenu className="items-end">
